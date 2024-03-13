9b479a73df30f369f757250141da6b31f5798add
Reemaiw <159250708+Reemaiw@users.noreply.github.com>
2024-03-13 15:08:48 +0300

    Add files via upload

---
+++ Lab_5/pro_2/app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+
+people = []
+
+class Person:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+def add_person(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        person = Person(username=username, password=password)
+        people.append(person)
+        return redirect('people')
+    return render(request, 'add.html')
+
+def show_people(request):
+    context = {'people': people}
+    return render(request, 'people.html', context)
